db5407d2c79e6ce9f9ba1ab284a964d6b2c8db1b
nedaakbari <nedaakbari555@gmail.com>
2024-11-30 20:21:29 +0330

    initialize

---
+++ src/test/java/com/samplesecurity/securities/SecuritiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samplesecurity.securities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
